7c45e551045a0e5008cf704f594fe3515d878d63
Harum Naseem <harum.naseem55@gmail.com>
2021-04-26 09:09:27 +0500

    update src

---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import { createStore } from 'vuex'
+
+import users from "./modules/users";
+import articles from "./modules/articles";
+
+export default createStore({
+  state: {},
+  mutations: {},
+  actions: {},
+  modules: {
+    users,
+    articles
+  }
+});
