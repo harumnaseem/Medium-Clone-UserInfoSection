2147516b47b85f61a56b5dfaa1a6be63513d4d04
Harum Naseem <harum.naseem55@gmail.com>
2021-04-26 09:42:14 +0500

    update src

---
+++ src/router/index.js
@@ -0,0 +1,39 @@
+import { createRouter, createWebHashHistory } from "vue-router"; 
+import Home from "../views/Home.vue"; 
+import Login from "../views/Login.vue"; 
+import Register from "../views/Register.vue"; 
+import Settings from "../views/Settings.vue";
+import Profile from "../views/Profile.vue";
+
+const routes = [
+ {
+     path: "/",
+     name: "Home",
+     component: Home 
+  }, 
+  {
+      path: "/login",    
+      name: "Login",
+      component: Login 
+   }, 
+   {    
+      path: "/register",
+      name: "Register",
+      component: Register 
+    }, 
+    {
+      path: "/settings",
+      name: "Settings",
+      component: Settings
+  },
+  {
+    path: "/:username",
+    name: "profile",
+    component: Profile
+  },
+]; 
+const router = createRouter({ 
+  history: createWebHashHistory(),
+  routes 
+}); 
+export default router;
---
+++ src/views/Profile.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="profile-page">
+    <div class="user-info">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12 col-md-10 offset-md-1">
+            <img src="https://i.imgur.com/wAoYezk.jpg" class="user-img" />
+            <h4>Kriss Kawa</h4>
+            <p>
+              Founder at Kriss, Developer, Content Writer
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+</div>
+</template>
